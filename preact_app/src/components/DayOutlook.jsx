54224fc60c23cdce3e11a36821b8eca1fc6a3b22
Jack <jack.robinson@banijay.com>
2024-03-13 13:46:23 +0000

    Laptop styling - 1024px breakpoint

--- preact_app/src/components/DayOutlook.jsx
+++ preact_app/src/components/DayOutlook.jsx
@@ -3,22 +3,20 @@ import WeatherIcon from "./WeatherIcon.jsx";
 function DayOutlook(props) {
     return (
         <div className='day-outlook data-container'>
-            {/*<div className='data-container-padding'>*/}
-                <p className='data-label'>{props.date}</p>
-                <div className='day-outlook-data'>
-                    <WeatherIcon iconText={props.icon} iconType='day-outlook-icon'/>
-                    <div className='day-outlook-temp-data'>
-                        <div className='min-temp'>
-                            <p className='data-label-bold'>Min</p>
-                            <p className='data-display-medium'>{props.tempMin}</p>
-                        </div>
-                        <div className='max-temp'>
-                            <p className='data-label-bold'>Max</p>
-                            <p className='data-display-medium'>{props.tempMax}</p>
-                        </div>
+            <p className='data-label'>{props.date}</p>
+            <div className='day-outlook-data'>
+                <WeatherIcon iconText={props.icon} iconType='day-outlook-icon'/>
+                <div className='day-outlook-temp-data'>
+                    <div className='min-temp'>
+                        <p className='data-label-bold'>Min</p>
+                        <p className='data-display-medium'>{props.tempMin}</p>
+                    </div>
+                    <div className='max-temp'>
+                        <p className='data-label-bold'>Max</p>
+                        <p className='data-display-medium'>{props.tempMax}</p>
                     </div>
                 </div>
-            {/*</div>*/}
+            </div>
         </div>
     );
 }
--- preact_app/src/components/ThreeDayOutlook.jsx
+++ preact_app/src/components/ThreeDayOutlook.jsx
@@ -4,10 +4,12 @@ function ThreeDayOutlook(props) {
     return (
         <div className='three-day-outlook'>
             <h3 className='section-label'>3 Day Outlook</h3>
-            {props.locationThreeDayOutlookData &&
-                props.locationThreeDayOutlookData.map(dayOutlookData => {
-                    return <DayOutlook {...dayOutlookData} city={props.city} class='resource'/>
-            })}
+            <div className='three-days-container'>
+                {props.locationThreeDayOutlookData &&
+                    props.locationThreeDayOutlookData.map(dayOutlookData => {
+                        return <DayOutlook {...dayOutlookData} city={props.city} class='resource'/>
+                    })}
+            </div>
         </div>
     );
 }
